b3ffb6f484409b07ce3e92e4fbc1ce6eddabde50
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-16 16:55:24 -0800

    add checkDatabase function indexedDB

--- public/js/indexedDB.js
+++ public/js/indexedDB.js
@@ -7,3 +7,39 @@ request.onupgradeneeded = function(event) {
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plin, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        }
+    };
+}
