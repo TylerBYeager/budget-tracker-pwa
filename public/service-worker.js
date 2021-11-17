2fe0e9ac8f12b0f2e67f5f261ce04bda0b209894
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-16 17:51:03 -0800

    add service-worker activate event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,9 +12,27 @@ const FILES_TO_CACHE = [
     "icons/icon-512x512.png"
 ];
 
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addALL(FILES_TO_CACHE))
     );
     self.skipWaiting();
 });
+
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
